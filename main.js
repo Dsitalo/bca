5ea43a47bdd76a48bc838aa8cab38f9c546618f2
Dsitalo <killsocialnetwork@gmail.com>
2020-01-28 22:44:03 +0200

    fix js

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
-let theory_block = document.querySelector(.study_info_th);
-let practice_block = document.querySelector(.study_info_pr);
+let theory_block = document.querySelector(".study_info_th");
+let practice_block = document.querySelector(".study_info_pr");
 
-let theory = document.querySelector(#theory_program);
-let practice = document.querySelector(#practice_program);
-theory.style.display = none;
-practice.style.display = none;
+let theory = document.querySelector("#theory_program");
+let practice = document.querySelector("#practice_program");
+theory.style.display = "none";
+practice.style.display = "none";
 
 theory_block.onclick = function () {
-    if (theory.style.display == none) {
-        theory.style.display = block;
+    if (theory.style.display == "none") {
+        theory.style.display = "block";
     } else {
-        theory.style.display = none;
+        theory.style.display = "none";
     }
 }
 
 practice_block.onclick = function () {
-    if (practice.style.display == none) {
-        practice.style.display = block;
+    if (practice.style.display == "none") {
+        practice.style.display = "block";
     } else {
-        practice.style.display = none;
+        practice.style.display = "none";
     }
 }
