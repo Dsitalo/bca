514abca31931384ef34bce360bd719e66b3fcc80
Dsitalo <killsocialnetwork@gmail.com>
2020-02-19 21:23:01 +0200

    prefixes

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ window.onload = function () {
         var change = function(e) {
           clear();
           e.target.classList.add('contacts-btn_checked');
-            let map = document.querySelector("#Map");
+            let map = document.querySelector("#map");
             if (e.target.id === "btnSecond") {
                 map.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2644.9363887449726!2d34.98288205148268!3d48.476933735340765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40dbe3b54ab6bf99%3A0x75c1cedbe58470ba!2z0L_RgNC-0YHQvy4g0KHQtdGA0LPQtdGPINCd0LjQs9C-0Y_QvdCwLCA2OCwg0JTQvdC40L_RgNC-LCDQlNC90LXQv9GA0L7Qv9C10YLRgNC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwgNDkwMDA!5e0!3m2!1sru!2sua!4v1578480966208!5m2!1sru!2sua";
             } else if (e.target.id === "btnThird") {
